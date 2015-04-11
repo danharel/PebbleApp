57789a457fb37a25791cb90ff8a6c85d7fb24177
Adam Liang <ayl292@nyu.edu>
2015-04-11 13:31:46 -0400

    Actually made jumping yay
    Only took 1 hour this time!

--- src/app.js
+++ src/app.js
@@ -1,51 +1,34 @@
 /**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
+ *Bitcamp 2015
+ *By: Dan Harel, Marlena Lui, Angela Bair, Adam Liang
  */
-//https://cloudpebble.net/ide/project/172772#
+
 var UI = require('ui');
 var Vector2 = require('vector2');
-//var ajax = require('ajax');
-
-//var player = require('Player.js');
-//var screen = require('Screen.js');
-
 var window = new UI.Window();
-var x = 100;
 
   var testRect = new UI.Rect({
-    position: new Vector2(x,x),
+    position: new Vector2(55,100),
     size: new Vector2(20,40)
   });
 
 window.add(testRect);
 window.show();
 
-//var pos = Rect.position();
-//pos.y -= 50;
-
-
 window.on('click', 'select', function() {
 	var pos = testRect.position();
   pos.y -= 50;
   
-  testRect.animate('position', pos, 1000);
+  var pos2 = testRect.position();
+  pos2.y += 50;
+  
+  //testRect.animate('position', pos, 1000);
+  
+  testRect.animate('position', new Vector2(55,20), 100).queue(function(next){
+  testRect.animate('position', new Vector2 (55,100), 100);
+  next();
+}); 
+
 });
 
-//var x = 0;
-/*setInterval(function() {
-	var main = new UI.Window({
-		title:'Pebble Run',
-		fullscreen: true
-	});
-	
-	main.add(new UI.Rect({
-		position: new Vector2(x,x),
-		size: new Vector2(20, 40)
-	}));
 
-	main.show();
-	
-	x+=10;
-}, 1000);*/
